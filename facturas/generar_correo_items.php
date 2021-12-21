629b382ab82cb222c8a360a892c303d5d7fd7193
Alex Rubiano <alexrubianoromero@gmail.com>
2021-12-21 12:23:15 -0500

    mejora envio de facturas por correo

---
+++ facturas/generar_correo_items.php
@@ -0,0 +1,58 @@
+<?php
+
+// echo '<pre>';
+// print_r($_REQUEST);
+// echo '</pre>';
+// die();
+include('../valotablapc.php');
+
+$sqlFacturaTraeIdOrden = "select * from $tabla11 where id_factura = '".$_REQUEST['id_factura']."' "; 
+$consultaIdOrden = mysql_query($sqlFacturaTraeIdOrden,$conexion);
+$arregloIdOrden = mysql_fetch_assoc($consultaIdOrden);
+$idOrden = $arregloIdOrden['id_orden'];
+$noFactura = $arregloIdOrden['numero_factura'];
+$placa = $arregloIdOrden['placa'];
+// echo '<br>'.$idOrden;
+// die();
+
+$sql_correo = "select cli.email as email from $tabla14 o
+inner join $tabla4 c on (c.placa = o.placa)
+inner join $tabla3 cli on (cli.idcliente=c.propietario)
+where o.id = '".$idOrden."'
+";
+$consulta_email= mysql_query($sql_correo,$conexion);
+$arreglo_email = mysql_fetch_assoc($consulta_email);
+$email = $arreglo_email['email'];
+// echo  '<br>'.$email;
+// die();
+
+
+$body = 'ELECTROINYECCION
+
+Te informa que el documento de tu reparacion lo puedes ver en el siguiente link
+
+Placa: '.$placa.'  Factura No: '.$noFactura.'
+
+Puedes ver tu orden de reparacion en el siguiente link:
+
+https://www.alexrubiano.com/electroinyeccion/factura/'.$_REQUEST['id_factura'].'
+
+
+Taller ELECTROINYECCION
+
+';
+// echo $body;
+// die();
+//echo '<br>'.$texto_items;
+
+//echo '<br>body<br>'.$body.'<br>fin de body</br>';
+//$body="prueba envio correo";
+/*
+$cuerpo_correo="crecion de orden";
+*/
+//////////////////////////////////////////////////////////////////	
+/////////////////enviar el correo 
+//mail($_REQUEST['email'],'MOTORCYCLE ROOM',$body,$headers); 
+include('enviar_correo_items.php');
+
+?>
--- facturas/muestre_factura.php
+++ facturas/muestre_factura.php
@@ -33,7 +33,7 @@ $consulta_ordenes = mysql_query($sql_muestre_ordenes,$conexion);
 	
 <?php
 echo '<table border= "1">';
-	echo '<td><h3>No FACTURA<h3></td><td><h3>PAGO/DEBE<h3></td><td><h3>TIPO FACTURA<h3></td><td><h3>ANULADO<h3></td><td><h3>Fecha</h3></td><td><h3>Placa/Identidad</h3></td><td><h3>modificar</h3></td>'; 
+	echo '<td><h3>No FACTURA<h3></td><td><h3>PAGO/DEBE<h3></td><td><h3>TIPO FACTURA<h3></td><td><h3>ANULADO<h3></td><td><h3>Fecha</h3></td><td><h3>Placa/Identidad</h3></td><td><h3>Correo</h3></td><td><h3>Impresion</h3></td>'; 
 		while($ordenes = mysql_fetch_array($consulta_ordenes))
 			{ 
 				$identificacion = $ordenes['2'];
@@ -65,6 +65,9 @@ echo '<table border= "1">';
 				echo '<td><h3>'.$identificacion.'</h3></td>';
 			    
 
+				echo  '<td><h3>';
+				echo '<a href="generar_correo_items.php?id_factura='.$ordenes['3'].'"  target = "_blank" >Correo</a>';
+				echo '</h3></td>'; 
 				echo  '<td><h3>';
 				echo '<a href="factura_imprimir.php?id_factura='.$ordenes['3'].'"  target = "_blank" >Vista Impresion</a>';
 				echo '</h3></td>'; 
